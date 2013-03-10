4484f5b8039f7c42302581c46688529f69340d55
siddhukrs <none@none>
2013-03-10 18:37:12 -0400

    almost highlights perfectly, prettify works!

--- test.php
+++ test.php
@@ -1,25 +1,25 @@
-
 <?php
 echo("<html><body>");
 //echo("<link href=\"/google-code-prettify/prettify.css\" type=\"text/css\" rel=\"stylesheet\" />");
-echo("<script type=\"text/javascript\" src=\"/google-code-prettify/prettify.js\"></script>");
-echo("<pre class=\"prettyprint\">");
-echo("<code class=\"language-java\">");
+echo("<script src=\"https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js\"></script>");
+echo("<pre class=prettyprint>");
 function printarray($array,$charat) {
 $temp=0;
 $flag=0;
    foreach ($array as $key => $value) {
 	$temp=$temp+sizeof(str_split($value));
 	//echo $temp;
-	if($temp<$charat and $flag==0)
+	if($temp<$charat-40 and $flag==0)
 	{
 		echo $value.PHP_EOL;
 	}
 	
 	else if($flag==0)
 	{
-		$flag=1;
-		echo "<font color=\"red\">".$value.PHP_EOL."</font>";
+		//$flag=1;
+		echo "<font  style=\"background-color: yellow\">".$value.PHP_EOL."</font>";
+		if($temp>=$charat)
+			$flag=1;
 	}
 	else if($flag==1)
 	{
@@ -52,7 +52,7 @@ return sizeof($temp);
 				$count=$count+1;
 				if($count==$codeid)
 					{
-					echo $code;
+					//echo $code;
 					$temp=explode(PHP_EOL,$code);
 					printarray($temp,$charat);
 					break;
@@ -62,7 +62,6 @@ return sizeof($temp);
 
 		}
 	}
-echo("</code>");
 echo("</pre>");
 echo("</body></html>");
 ?>
