36bbe7ea7d2031e546a04067c8fb0213dbd60423
Siddharth Subramanian <siddhukrs@gmail.com>
2013-06-04 17:21:25 -0400

    changed to url from file upload

--- extract/file_uploader.php
+++ extract/file_uploader.php
@@ -1,27 +1,28 @@
 <?php
-if ($_FILES["file"]["error"] > 0)
-  {
-  echo "Error: " . $_FILES["file"]["error"] . "<br>";
-  }
-else
-  {
-  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-  echo "Type: " . $_FILES["file"]["type"] . "<br>";
-  echo "Size: " . ($_FILES["file"]["size"] / 1048576) . " MB<br>";
-  //echo "Stored in: " . $_FILES["file"]["tmp_name"];
-if (file_exists($_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],$_FILES["file"]["name"]);
-      }
 
-exec('java -jar xml_zip_gen.jar '.$_FILES["file"]["name"],$output_array);
+$file = 'https://dl.dropboxusercontent.com/u/67596885/android.jar';
+$pieces=explode('/',$file);
+foreach($pieces as $piece)
+	$filename=$piece;
+//echo $filename
+$newfile = $_SERVER['DOCUMENT_ROOT'] .'/snippet/extract/jars/'.$filename.'';
+exec('mkdir jars');
+echo $newfile;
+if (copy($file, $newfile) ) {
+    echo "Copy success!";
+}else{
+    echo "Copy failed.";
+}
+//  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+//  echo "Type: " . $_FILES["file"]["type"] . "<br>";
+//  echo "Size: " . ($_FILES["file"]["size"] / 1048576) . " MB<br>";
+//echo "Stored in: " . $_FILES["file"]["tmp_name"];
+
+exec('java -jar xml_zip_gen.jar jars/'.$filename,$output_array);
 $output="";
 foreach($output_array as $c)
 {
+echo $c;
 $output=$output.$c;
 }
 
@@ -34,5 +35,4 @@ foreach($pieces as $piece)
 $link=$link.$piece.'/';
 }
 echo "<br><br><a href=\"".$link.$output."\">"."Click here to download the file"."</a>";
-}
 ?>
